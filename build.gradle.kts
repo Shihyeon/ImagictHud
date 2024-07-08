570fe8347b96ee47bd1ae56b04ce68187903e0b4
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-07-08 19:18:33 +0900

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ object Constants {
     const val YACL_VERSION: String = "3.5.0+1.21-fabric"
 
     // https://semver.org/
-    const val MOD_VERSION: String = "1.2.2"
+    const val MOD_VERSION: String = "1.2.3"
     const val MOD_TYPE: String = "release" // release/beta/alpha
 }
 
