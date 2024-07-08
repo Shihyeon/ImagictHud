e03db922ce47abae327ff0918e90db9fe0173637
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-07-08 13:29:29 +0900

    feat: Add hud scale config

--- src/main/java/kr/shihyeon/imagicthud/hud/Hud.java
+++ src/main/java/kr/shihyeon/imagicthud/hud/Hud.java
@@ -7,6 +7,7 @@ import kr.shihyeon.imagicthud.util.LayoutUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.client.util.math.MatrixStack;
 
 public class Hud {
     private static PlayerListEntry playerListEntry;
@@ -21,6 +22,8 @@ public class Hud {
             int height = 13;
             int lineSpacing = config.lebelTextLineSpacing;
 
+            float scale = config.scale;
+
             int frameColor = ColorHelper.getLabelFrameColor();
             int backgroundColor = ColorHelper.getLabelBackgroundColor();
             int textColor = ColorHelper.getLabelTextColor();
@@ -31,12 +34,18 @@ public class Hud {
                 playerListEntry = client.player.networkHandler.getPlayerListEntry(client.player.getUuid());
             }
 
+            MatrixStack matrixStack = context.getMatrices();
+            matrixStack.push();
+            matrixStack.scale(scale, scale, 1.0f);
+
             if (config.enableHeadHud) {
                 ResourceGui.renderHead(context, playerListEntry, posX, posY);
                 posX = LayoutUtil.getLabelPosX(client);
             }
 
             Label.renderLabel(context, client, playerListEntry, posX, posY, width, height, lineSpacing, frameColor, backgroundColor, textColor, shadow, center);
+
+            matrixStack.pop();
         }
     }
 }
