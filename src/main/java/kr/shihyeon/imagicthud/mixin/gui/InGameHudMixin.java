fc0b7d670c397f56d0d0066914c48407b7b9d68c
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-08-01 23:09:16 +0900

    chore

--- src/main/java/kr/shihyeon/imagicthud/mixin/gui/InGameHudMixin.java
+++ src/main/java/kr/shihyeon/imagicthud/mixin/gui/InGameHudMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public class InGameHudMixin {
+public abstract class InGameHudMixin {
 
     @Shadow @Final
     private MinecraftClient client;
